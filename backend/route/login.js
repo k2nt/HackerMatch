f108e0f9d0a2be4d601b7aec2b11e79892f885b2
Thisausername99 <cody_hoang99@yahoo.com>
2020-12-20 04:26:44 +0700

    update login to match schema

--- backend/route/login.js
+++ backend/route/login.js
@@ -15,15 +15,17 @@ const generateAuthToken = () => {
  * Return { success: true } on success, and { success: false, code: <HTTP status code>, errmsg <String> } otherwise
  */
 regRouter.post('/submit-form', async (req, res) => {
-    const inputEmail = req.body.user.personal.email;
-    const inputPassword = req.body.user.password;
+    const inputEmail = req.body.email;
+    const inputPassword = req.body.password;
 
-    var data = await schema.findOne({ email: inputEmail });
 
+    var data = await schema.findOne({ "user.personal.email": inputEmail });
+
+    console.log(data);
     if (data == null) {
         res.json({ success: false, error: 404, errmsg: "email not in database" });
     } else {
-        if (data.password === inputPassword) {
+        if (data.user.password === inputPassword) {
             res.json({ success: true });
         } else {
             res.json({ success: false, error: 400, errmsg: "input password does not match database"});
