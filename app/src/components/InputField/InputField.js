a75fd80e070e3caffbaa76f4567cc31ab04f584a
Khai Nguyen <tuankhai2k@gmail.com>
2020-12-20 19:02:00 +0700

    finish search bar for marketplace page, minor bug fixes

--- app/src/components/InputField/InputField.js
+++ app/src/components/InputField/InputField.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './InputField.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 const InputField = ({
 	fieldTitle="Title",
 	placeholder="placeholder",
+	icon=null,
+	iconSize="1x",
 	type="text",
 	value,
 	onChangeValue,
@@ -12,19 +16,32 @@ const InputField = ({
 }) => {
 
 	return (
-		<div className='input-field-container'>
-			<h5 className='field-title'>{fieldTitle}</h5>
-			<input
-				className='input-field'
-				type={type}
-				placeholder={placeholder}
-				autoFocus={autoFocus}
-				value={value}
-				onChange={onChangeValue}
-				required={required}
-			/>
+		<div className='input-field-wrapper'>
+			{
+				fieldTitle !== "" ? 
+					<h5 className='field-title'>{fieldTitle}</h5>
+				:
+					<div/>
+			}
+			<div className='input-field-container'>
+				{
+					icon !== null ? 
+						<FontAwesomeIcon className='icon' icon={icon} size={iconSize} />
+					:
+						<div/>
+				}
+				<input
+					className='input-field'
+					type={type}
+					placeholder={placeholder}
+					autoFocus={autoFocus}
+					value={value}
+					onChange={onChangeValue}
+					required={required}
+				/>
+			</div>
 		</div>
-	);
+	); 
 }
 
 export default InputField;
--- app/src/pages/MarketplacePage/MarketplacePage.js
+++ app/src/pages/MarketplacePage/MarketplacePage.js
@@ -1,9 +1,69 @@
 import React from 'react';
+import './MarketplacePage.css';
+import InputField from '../../components/InputField/InputField';
+import Button from '../../components/Button/Button';
+import searchIcon from '../../assets/images/search-icon.png';
+import { faSearch, faBell } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import dummyAvatar from '../../assets/images/johhdoe.jpeg';
+
+const SearchNavBar = () => {
+	return (
+		<div className='search-nav-bar-wrapper'>
+			<div className='search-nav-bar-container'>
+				<form 
+					className='search-bar-wrapper'
+					onSubmit={{}}
+				>
+					<InputField
+						fieldTitle=""
+						placeholder=""
+						icon={faSearch}
+						iconSize="1x"
+						
+					/>
+				</form>
+				<div className='btn-notification'>
+					<Button
+						btnStyle="btn--transparent"
+						btnSize="btn--wrap"
+						children={
+							<FontAwesomeIcon 
+								icon={faBell}
+								style={{
+									fontSize: "2.2rem"
+								}}
+							/>
+						}
+					/>
+				</div>
+				<div className='btn-avatar'>
+					<Button
+						btnStyle="btn--transparent"
+						btnSize="btn--wrap"
+						children={
+							<img 
+								alt="avatar" 
+								src={dummyAvatar} 
+								style={{ 
+									width: "3.2rem",
+									height: "3.2rem",
+									borderRadius: "100px"
+								}}
+							/>
+						}
+					/>
+				</div>
+			</div>
+		</div>
+
+	);
+}
 
 const MarketplacePage = () => {
 	return (
 		<div className='marketplace-page'>
-			<h1>Marketplace</h1>
+			<SearchNavBar />
 		</div>
 	);
 }
