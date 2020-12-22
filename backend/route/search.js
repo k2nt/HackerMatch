ac28cfcbb41d1833b856d880face6262764b8f7f
Thisausername99 <cody_hoang99@yahoo.com>
2020-12-23 03:15:45 +0700

    fix get user by email

--- backend/route/search.js
+++ backend/route/search.js
@@ -5,13 +5,13 @@ const verify = require('./tokenVerify');
 
 queryRoute.get('/get', verify, async (req,res)=> {
     
-    let data = await schema.findOne({ "user.personal.email": req.body.email }); //retrieve user from database
+    let user = await schema.findOne({ "personal.email": req.body.email }); //retrieve user from database
 
     // console.log(data);
-    if (data == null) {
+    if (!user) {
         res.json({ success: false, error: 404, errmsg: "email not in database" });
     } else {
-        res.json(data);   
+        res.json(user);   
     }
 });
 
